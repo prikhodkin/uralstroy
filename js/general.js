0f7ada7a451fa9bce5ea12c8b8ce4e682b053946
prikhodkin <info@prikhodkin.com>
2020-05-12 02:15:21 +0500

    Updates

--- js/general.js
+++ js/general.js
@@ -1,2 +1,15 @@
 "use strict";
+
+$('.menu__btn').click(function () {
+  $('.menu__hamburger').toggleClass('menu__hamburger--active');
+});
+$('.header__loupe').on('click', function () {
+  $('.header__search-row').slideToggle();
+});
+$('.header__search-close--button').on('click', function () {
+  $('.header__search-row').slideUp();
+});
+$('.header__search-circle').on('click', function () {
+  $('.header__search-row input').val("");
+});
 //# sourceMappingURL=maps/general.js.map
