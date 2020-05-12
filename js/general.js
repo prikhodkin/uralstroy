58d057288a218968a4bf72215aac2fda2c4be5f8
prikhodkin <info@prikhodkin.com>
2020-05-13 02:02:05 +0500

    Updates

--- js/general.js
+++ js/general.js
@@ -12,4 +12,14 @@ $('.header__search-close--button').on('click', function () {
 $('.header__search-circle').on('click', function () {
   $('.header__search-row input').val("");
 });
+var inputs = document.querySelectorAll('.input-f');
+Array.prototype.forEach.call(inputs, function (input) {
+  var label = input.nextElementSibling,
+      labelVal = label.querySelector('.input__text').innerText;
+  input.addEventListener('change', function (e) {
+    var countFiles = '';
+    if (this.files && this.files.length >= 1) countFiles = this.files.length;
+    if (countFiles) label.querySelector('.input__text').innerText = 'Выбрано файлов: ' + countFiles;else label.querySelector('.input__text').innerText = labelVal;
+  });
+});
 //# sourceMappingURL=maps/general.js.map
