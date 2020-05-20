c6c74beedc334373a321dbd65cc3432801442e3a
prikhodkin <info@prikhodkin.com>
2020-05-21 02:45:26 +0500

    Закончил страницу Категория

---
+++ src/blocks/category/category.js
@@ -0,0 +1,13 @@
+modules.define('category', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
