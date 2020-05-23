4048393f8f7c6ae3decbabcdd03b4b44dc9f37ab
prikhodkin <info@prikhodkin.com>
2020-05-24 01:43:46 +0500

    Updates

--- js/general.js
+++ js/general.js
@@ -41,4 +41,26 @@ $('.product__step--max').on('click', function (evt) {
 $('.product__show').on('click', function () {
   $('.product__info--hide').slideToggle();
 });
+$('.about__header--mobile').on('click', function () {
+  $('.about__header--mobile').toggleClass('about__header--active');
+  $('.about__menu-wrapper').slideToggle();
+});
+$('.about__header--company').on('click', function () {
+  $('.about__header--company').toggleClass('about__header--active');
+  $('.about__menu--company').slideToggle();
+});
+$('.about__header--clients').on('click', function () {
+  $('.about__header--clients').toggleClass('about__header--active');
+  $('.about__menu--clients').slideToggle();
+});
+$('.about__slider').slick({
+  dots: false,
+  accessibility: false,
+  prevArrow: '.about__slider-arrow--prev',
+  nextArrow: '.about__slider-arrow--next'
+});
+$('.about__slider-length').html('0' + $(".about__slider").slick("getSlick").slideCount);
+$(".about__slider").on("afterChange", function (event, slick, currentSlide, nextSlide) {
+  $(".about__slider-current").html('0' + (currentSlide + 1));
+});
 //# sourceMappingURL=maps/general.js.map
